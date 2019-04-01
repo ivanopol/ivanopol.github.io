d4574606cbac4213603d0d629cbf9de32bd30ed2
ivanopol <ivanopol777@mail.ru>
2019-04-01 18:10:57 +0500

    Make responsible greeting block for desktops

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,23 @@
               a(href="#experience") Опыт работы
               a(href="#skills") Навыки
               a(href="#contacts") Контакты
-          div.block.appeal
-            h1 Дмитрий Иванов
-            h2 Backend Developer
-            div.underline
-            p Здравстуйте, меня зовут Дмитрий. Я&nbsp;занимаюсь
-              |  веб-разработкой уже много лет и&nbsp;мне это занятие очень
-              | нравится. Сейчас я&nbsp;ищу работу на&nbsp;позицию
-              | backend-developer или fullstack-developer.
-            a(href="./assets/pdf/Portfolio_SPA.pdf"
-             class="button"
-             ) Скачать портфолио
-              span.icon_pdf
-          div.block.illustration
-            img(
-              src="./assets/svg/picture.svg"
-            )
+          div.block_wrapper
+            div.block.appeal
+              h1 Дмитрий Иванов
+              h2 Backend Developer
+              div.underline
+              p Здравстуйте, меня зовут Дмитрий. Я&nbsp;занимаюсь
+                |  веб-разработкой уже много лет и&nbsp;мне это занятие очень
+                | нравится. Сейчас я&nbsp;ищу работу на&nbsp;позицию
+                | backend-developer или fullstack-developer.
+              a(href="./assets/pdf/Portfolio_SPA.pdf"
+              class="button"
+              ) Скачать портфолио
+                span.icon_pdf
+            div.block.illustration
+              img(
+                src="./assets/svg/picture.svg"
+              )
 </template>
 
 <script>
