6f7902e855a776756ee2ab4e853fd7d716f0c3a8
ivanopol <ivanopol777@mail.ru>
2019-04-15 10:11:53 +0500

    Fix scrooll-progress bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Siema from 'vue2-siema'
-import VueScrollProgress from 'vue-scroll-progress'
+import VueScrollProgress from './scroll-progress.js'
 import SmoothScrollbar from 'vue-smooth-scrollbar'
 
 Vue.use(Router)
---
+++ src/router/scroll-progress.js
@@ -0,0 +1,85 @@
+var VueScrollProgress = {
+  install: function install (Vue, options) {
+    Vue.mixin({
+      mounted: function mounted () {
+        var progressContainerEl,
+          progressEl,
+          windowScrollPixels,
+          windowScrollPercents,
+          height
+
+        progressContainerEl = document.createElement('div')
+        progressContainerEl.id = 'progress-container-el'
+        progressContainerEl.style.position = 'fixed'
+        progressContainerEl.style.width = '100%'
+        progressContainerEl.style.height = '4px'
+        progressContainerEl.style.backgroundColor = 'transparent'
+        progressContainerEl.style.left = 0
+        progressContainerEl.style.right = 0
+        progressContainerEl.style.top = 0
+        progressContainerEl.style.zIndex = 99999
+
+        progressEl = document.createElement('div')
+        progressEl.id = 'progress-el'
+        progressEl.style.width = '0px'
+        progressEl.style.height = '4px'
+        progressEl.style.backgroundColor = '#42b883'
+
+        windowScrollPixels = 0
+        windowScrollPercents = 0
+
+        function getHeight () {
+          // height = (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+          height = document.getElementById('greeting').clientHeight
+          height += document.getElementById('history').clientHeight / 3
+          height += document.getElementById('experience').clientHeight
+          height += document.getElementById('skills').clientHeight
+          height += document.getElementById('books').clientHeight
+          height += document.getElementById('contacts').clientHeight
+          height -= Math.ceil(document.documentElement.clientHeight + (document.documentElement.clientHeight / 100 * 19.33))
+        }
+
+        const debounce = (func, wait, immediate) => {
+          var timeout
+          return () => {
+            const context = this
+            const args = arguments
+            const later = function () {
+              timeout = null
+              if (!immediate) func.apply(context, args)
+            }
+            const callNow = immediate && !timeout
+            clearTimeout(timeout)
+            timeout = setTimeout(later, wait)
+            if (callNow) func.apply(context, args)
+          }
+        }
+
+        var h = document.body
+        h.addEventListener('resize', debounce(() => {
+          getHeight()
+        }, 200, false), false)
+
+        getHeight()
+
+        window.addEventListener('scroll', () => {
+          windowScrollPixels = (document.body.scrollTop ||
+            document.documentElement.scrollTop)
+          windowScrollPercents = (windowScrollPixels / height) * 100
+          progressEl.style.width = windowScrollPercents + '%'
+        })
+
+        if (!document.getElementById('progress-container-el')) {
+          progressContainerEl.appendChild(progressEl)
+          document.body.appendChild(progressContainerEl)
+        }
+      }
+    })
+  }
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.VueScrollProgress = VueScrollProgress
+}
+
+export default VueScrollProgress
